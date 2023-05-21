c00c61791013581064c46d9272a90e718446d760
Admin <Admin@DESKTOP-QQR48TL>
2023-05-21 20:11:20 +0600

    Добавьте файлы проекта.

---
+++ HomeWorkMiddlewarMVC/Models/MiddlewareExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public static class MiddlewareExtensions
+{
+    public static IApplicationBuilder UseRequestLoggingMiddleware(this IApplicationBuilder app, string logFilePath)
+    {
+        return app.UseMiddleware<RequestLoggingMiddleware>(logFilePath);
+    }
+}
+
+public class Startup
+{
+    // ...
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // ...
+
+        string logFilePath = "requests.log";
+        app.UseRequestLoggingMiddleware(logFilePath);
+
+        // ...
+    }
+}
---
+++ HomeWorkMiddlewarMVC/Models/RequestLoggingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+public class RequestLoggingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly string _logFilePath;
+
+    public RequestLoggingMiddleware(RequestDelegate next, string logFilePath)
+    {
+        _next = next;
+        _logFilePath = logFilePath;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        string requestPath = context.Request.Path;
+        string logMessage = $"[{DateTime.Now}] Requested path: {requestPath}";
+
+        using (StreamWriter writer = new StreamWriter(_logFilePath, append: true))
+        {
+            await writer.WriteLineAsync(logMessage);
+        }
+
+        await _next(context);
+    }
+}
